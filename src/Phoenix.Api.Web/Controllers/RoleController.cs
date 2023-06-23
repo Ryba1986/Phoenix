0f745b2408abbec5e5645a34230209286dfd5211
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2023-06-23 18:33:51 +0200

    Add roles

---
+++ src/Phoenix.Api.Web/Controllers/RoleController.cs
@@ -0,0 +1,45 @@
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Phoenix.Api.Web.Attributes;
+using Phoenix.Api.Web.Controllers.Base;
+using Phoenix.Models.Roles.Commands;
+using Phoenix.Models.Roles.Queries;
+using Phoenix.Shared.Enums.Roles;
+
+namespace Phoenix.Api.Web.Controllers
+{
+   public sealed class RoleController : ApiControllerBase
+   {
+      public RoleController(IMediator mediator) : base(mediator)
+      {
+      }
+
+      [HttpGet]
+      [Authorization(Permission.Role, AccessLevel.Read)]
+      public async Task<IActionResult> GetRoles()
+      {
+         return await HandleAsync(new GetRolesQuery());
+      }
+
+      [HttpGet]
+      public async Task<IActionResult> GetRoleDictionary()
+      {
+         return await HandleAsync(new GetRoleDictionaryQuery());
+      }
+
+      [HttpPost]
+      [Authorization(Permission.Role, AccessLevel.Write)]
+      public async Task<IActionResult> CreateRole([FromBody] CreateRoleCommand request)
+      {
+         return await HandleAsync(request);
+      }
+
+      [HttpPost]
+      [Authorization(Permission.Role, AccessLevel.Write)]
+      public async Task<IActionResult> UpdateRole([FromBody] UpdateRoleCommand request)
+      {
+         return await HandleAsync(request);
+      }
+   }
+}
