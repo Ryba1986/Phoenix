01e74d34bfa7263a7a38a62dc315b155306de3f1
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2023-06-30 21:11:04 +0200

    Client publish command

--- scripts/Publish.ps1
+++ scripts/Publish.ps1
@@ -1,2 +1,3 @@
 dotnet publish "./src/Phoenix.Api.Client/Phoenix.Api.Client.csproj" -o "./dist/Phoenix.Api.Client" -c Release
 dotnet publish "./src/Phoenix.Api.Web/Phoenix.Api.Web.csproj" -o "./dist/Phoenix.Api.Web" -c Release
+dotnet publish "./src/Phoenix.Client/Phoenix.Client.csproj" -o "./dist/Phoenix.Client" -c Release
