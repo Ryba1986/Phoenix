7007c80d92a24a848e7b25dd3addc558a0db7325
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2023-07-01 07:06:14 +0200

    Extensions for reports

---
+++ src/Phoenix.Services/Extensions/ExcelExtensions.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using ClosedXML.Excel;
+using Phoenix.Shared.Enums.Reports;
+
+namespace Phoenix.Services.Extensions
+{
+   internal static class ExcelExtensions
+   {
+      public static IXLWorkbook GetReportTemplate()
+      {
+         string templatePath = $"{AppContext.BaseDirectory}Reports{Path.DirectorySeparatorChar}Templates{Path.DirectorySeparatorChar}PlcReport_{CultureInfo.CurrentCulture.Name}.xlsx";
+         return new XLWorkbook(templatePath);
+      }
+
+      public static IEnumerable<string> GetSheetNames(this IXLWorksheets sheets)
+      {
+         return sheets.Select(x => x.Name);
+      }
+
+      public static IXLWorksheet CloneSheet(this IXLWorksheets sheets, ReportType type, string newSheetName)
+      {
+         return sheets
+            .First(x => x.Name == type.ToString())
+            .CopyTo(newSheetName);
+      }
+
+      public static void RemoveSheets(this IXLWorksheets sheets, IEnumerable<string> sheetNames)
+      {
+         foreach (string name in sheetNames)
+         {
+            sheets.Delete(name);
+         }
+         if (sheets.Count == 0)
+         {
+            sheets.Add("NO DATA");
+         }
+      }
+   }
+}
