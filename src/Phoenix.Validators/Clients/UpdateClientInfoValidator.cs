f668621eaadb2359c252ed7ac246e3765054844e
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2023-07-03 21:56:25 +0200

    Fix validator

--- src/Phoenix.Validators/Clients/UpdateClientInfoValidator.cs
+++ src/Phoenix.Validators/Clients/UpdateClientInfoValidator.cs
@@ -9,12 +9,12 @@ namespace Phoenix.Validators.Clients
       public UpdateClientInfoValidator()
       {
          RuleFor(x => x.Hostname)
-            .NotEmpty().WithMessage(Translations.Validator_Hostname_Invalid)
-            .Length(60, 66).WithMessage(Translations.Validator_Hostname_Invalid);
+            .NotNull().WithMessage(Translations.Validator_Hostname_Invalid)
+            .MaximumLength(66).WithMessage(Translations.Validator_Hostname_Invalid);
 
          RuleFor(x => x.ClientVersion)
-            .NotEmpty().WithMessage(Translations.Validator_ClientVersion_Invalid)
-            .Length(3, 20).WithMessage(Translations.Validator_ClientVersion_Invalid);
+            .NotNull().WithMessage(Translations.Validator_ClientVersion_Invalid)
+            .MaximumLength(20).WithMessage(Translations.Validator_ClientVersion_Invalid);
       }
    }
 }
