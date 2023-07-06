bb017b91cd84508b2c346c7fe159772859337cde
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2023-07-06 22:42:27 +0200

    Refactoring reports (part 9)

--- src/Phoenix.Services/Handlers/Reports/Queries/GetReportHandler.cs
+++ src/Phoenix.Services/Handlers/Reports/Queries/GetReportHandler.cs
@@ -4,8 +4,11 @@ using System.Threading;
 using System.Threading.Tasks;
 using Autofac.Features.Indexed;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using OfficeOpenXml;
+using Phoenix.Models.Devices.Dto;
 using Phoenix.Models.Reports.Queries;
 using Phoenix.Services.Extensions;
 using Phoenix.Services.Handlers.Base;
@@ -33,12 +36,8 @@ namespace Phoenix.Services.Handlers.Reports.Queries
          ITypeProcessor typeProcessor = _typeProcessors[request.Type];
          using ExcelPackage ep = ExcelExtensions.GetReportTemplate();
 
-         IReadOnlyCollection<string> templateSheetNames = ep.Workbook.Worksheets
-            .GetSheetNames()
-            .ToArray();
-
          ep.Workbook.Worksheets.RemoveTemplateRows(typeProcessor.RemoveTemplateRowCount);
-         ep.Workbook.Worksheets.RemoveSheets(templateSheetNames);
+         await CreateSheetsAsync(ep.Workbook.Worksheets, request, typeProcessor, cancellationToken);
 
          return new()
          {
@@ -46,6 +45,40 @@ namespace Phoenix.Services.Handlers.Reports.Queries
             Type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
             Data = ep.GetAsByteArray(),
          };
+      }
+
+      private async Task CreateSheetsAsync(ExcelWorksheets sheets, GetReportQuery request, ITypeProcessor typeProcessor, CancellationToken cancellationToken)
+      {
+         IReadOnlyCollection<DeviceReportDto> devices = await _uow.Device
+            .AsNoTracking()
+            .Include(x => x.Location)
+            .Where(x =>
+               x.IncludeReport &&
+               x.Location.IncludeReport
+            )
+            .OrderBy(x => x.Location.Name)
+            .ThenBy(x => x.Name)
+            .ProjectTo<DeviceReportDto>(_mapper.ConfigurationProvider)
+            .ToArrayAsync(cancellationToken);
+
+         if (devices.Count == 0)
+         {
+            return;
+         }
+
+         IEnumerable<Task> plcTasks = devices
+            .GroupBy(x => x.PlcType)
+            .Select(x => _plcProcessors[x.Key].FillDataAsync(sheets, request.Date, x.ToArray(), typeProcessor, cancellationToken));
+
+         await Task.WhenAll(plcTasks);
+
+         IReadOnlyCollection<string> sheetNamesToRemove = sheets
+           .GetSheetNames()
+           .ToArray();
+
+         //TODO: combine sheets to locations
+
+         sheets.RemoveSheets(sheetNamesToRemove);
       }
    }
 }
