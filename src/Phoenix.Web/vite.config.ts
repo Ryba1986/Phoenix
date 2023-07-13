c64dd9d0e9152047ecf1d3e1d8fc3d8159cf9235
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2023-07-13 21:35:14 +0200

    Config for build

--- src/Phoenix.Web/vite.config.ts
+++ src/Phoenix.Web/vite.config.ts
@@ -3,5 +3,11 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   build: {
+      chunkSizeWarningLimit: 4096,
+      emptyOutDir: true,
+      outDir: "../../dist/Phoenix.Web",
+      reportCompressedSize: false,
+   },
    plugins: [vue()],
 });
