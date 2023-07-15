e8ebb27d47ef4ebcf17135e9cdfb6530a592125f
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2023-07-15 12:07:35 +0200

    Setup PrimeVue

--- src/Phoenix.Web/src/main.ts
+++ src/Phoenix.Web/src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from "vue";
+import PrimeVue from "primevue/config";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import "primevue/resources/themes/lara-light-teal/theme.css";
+import "primevue/resources/primevue.css";
+import "primeflex/primeflex.scss";
+import "primeicons/primeicons.css";
+
+createApp(App).use(PrimeVue).mount("#app");
