150ccfce6119676d2791ff82bc913dfbd87d5419
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2023-07-22 22:04:06 +0200

    Api for plcs

---
+++ src/Phoenix.Web/src/api/climatixApi.ts
@@ -0,0 +1,13 @@
+import { getAsync } from "../helpers/requestHelper";
+import { ClimatixChartDto } from "../models/api/plcs/climatixs/dto/climatixChartDto";
+import { ClimatixDto } from "../models/api/plcs/climatixs/dto/climatixDto";
+import { GetClimatixChartQuery } from "../models/api/plcs/climatixs/queries/getClimatixChartQuery";
+import { GetClimatixLastQuery } from "../models/api/plcs/climatixs/queries/getClimatixLastQuery";
+
+export function getClimatixChartAsync(request: GetClimatixChartQuery): Promise<Array<ClimatixChartDto>> {
+   return getAsync<Array<ClimatixChartDto>>("climatix/getClimatixChart", request);
+}
+
+export function getClimatixLastAsync(request: GetClimatixLastQuery): Promise<ClimatixDto> {
+   return getAsync<ClimatixDto>("kamstrup/getClimatixLast", request);
+}
---
+++ src/Phoenix.Web/src/api/kamstrupApi.ts
@@ -0,0 +1,13 @@
+import { getAsync } from "../helpers/requestHelper";
+import { KamstrupChartDto } from "../models/api/plcs/meters/dto/kamstrupChartDto";
+import { KamstrupDto } from "../models/api/plcs/meters/dto/kamstrupDto";
+import { GetKamstrupChartQuery } from "../models/api/plcs/meters/queries/getKamstrupChartQuery";
+import { GetKamstrupLastQuery } from "../models/api/plcs/meters/queries/getKamstrupLastQuery";
+
+export function getKamstrupChartAsync(request: GetKamstrupChartQuery): Promise<Array<KamstrupChartDto>> {
+   return getAsync<Array<KamstrupChartDto>>("kamstrup/getKamstrupChart", request);
+}
+
+export function getKamstrupLastAsync(request: GetKamstrupLastQuery): Promise<KamstrupDto> {
+   return getAsync<KamstrupDto>("kamstrup/getKamstrupLast", request);
+}
