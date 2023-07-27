4aae7e5d6635bb9568bc29b992b323130b64697b
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2023-07-27 18:41:18 +0200

    Migrate to devextreme

--- src/Phoenix.Web/src/main.ts
+++ src/Phoenix.Web/src/main.ts
@@ -1,15 +1,12 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import PrimeVue from "primevue/config";
 import App from "./App.vue";
 import { i18n } from "./languages";
 import { componentPlugin } from "./plugins/componentPlugin";
 import router from "./router";
 
-import "primevue/resources/themes/lara-light-teal/theme.css";
-import "primevue/resources/primevue.css";
-import "primeflex/primeflex.scss";
-import "primeicons/primeicons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "devextreme/dist/css/dx.material.teal.light.compact.css";
 import "./styles/main.scss";
 
-createApp(App).use(router).use(createPinia()).use(PrimeVue).use(componentPlugin).use(i18n).mount("#app");
+createApp(App).use(router).use(createPinia()).use(componentPlugin).use(i18n).mount("#app");
