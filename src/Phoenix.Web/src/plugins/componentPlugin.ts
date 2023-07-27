dfcbdbf74430cb8dd37e57f107d730892c090460
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2023-07-27 19:03:02 +0200

    Basic components

--- src/Phoenix.Web/src/plugins/componentPlugin.ts
+++ src/Phoenix.Web/src/plugins/componentPlugin.ts
@@ -1,7 +1,17 @@
 import { App, Plugin } from "vue";
 
+import { DxButton } from "devextreme-vue/button";
+import { DxDateBox } from "devextreme-vue/date-box";
+import { DxLoadPanel } from "devextreme-vue/load-panel";
+import { DxSelectBox } from "devextreme-vue/select-box";
+import { DxTextBox } from "devextreme-vue/text-box";
+
 export const componentPlugin: Plugin = {
    install(app: App<Element>): void {
-      // app.component("",);
+      app.component("DxButton", DxButton);
+      app.component("DxDateBox", DxDateBox);
+      app.component("DxLoadPanel", DxLoadPanel);
+      app.component("DxSelectBox", DxSelectBox);
+      app.component("DxTextBox", DxTextBox);
    },
 };
