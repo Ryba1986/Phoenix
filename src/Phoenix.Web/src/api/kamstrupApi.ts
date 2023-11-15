09dc44d41fae562a48efb8eab358875a9138290e
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2023-11-15 19:50:36 +0100

    Normalize location

--- src/Phoenix.Web/src/api/kamstrupApi.ts
+++ src/Phoenix.Web/src/api/kamstrupApi.ts
@@ -1,8 +1,8 @@
 import { getAsync } from '../helpers/requestHelper';
-import { KamstrupChartDto } from '../models/api/plcs/kamstrups/dto/kamstrupChartDto';
-import { KamstrupDto } from '../models/api/plcs/kamstrups/dto/kamstrupDto';
-import { GetKamstrupChartDayQuery } from '../models/api/plcs/kamstrups/queries/getKamstrupChartDayQuery';
-import { GetKamstrupLastQuery } from '../models/api/plcs/kamstrups/queries/getKamstrupLastQuery';
+import { KamstrupChartDto } from '../models/api/plcs/meters/dto/kamstrupChartDto';
+import { KamstrupDto } from '../models/api/plcs/meters/dto/kamstrupDto';
+import { GetKamstrupChartDayQuery } from '../models/api/plcs/meters/queries/getKamstrupChartDayQuery';
+import { GetKamstrupLastQuery } from '../models/api/plcs/meters/queries/getKamstrupLastQuery';
 
 export function getKamstrupChartDayAsync(request: GetKamstrupChartDayQuery): Promise<Array<KamstrupChartDto>> {
    return getAsync<Array<KamstrupChartDto>>('kamstrup/getKamstrupChartDay', request);
