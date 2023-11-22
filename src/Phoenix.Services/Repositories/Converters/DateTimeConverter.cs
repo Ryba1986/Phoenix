f754dc0f959baeed52cfd0b7569e1777f2128d24
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2023-11-22 22:00:01 +0100

    Change conversion

--- src/Phoenix.Services/Repositories/Converters/DateTimeConverter.cs
+++ src/Phoenix.Services/Repositories/Converters/DateTimeConverter.cs
@@ -5,7 +5,7 @@ namespace Phoenix.Services.Repositories.Converters
 {
    internal sealed class DateTimeConverter : ValueConverter<DateTime, DateTime>
    {
-      public DateTimeConverter() : base(x => DateTime.SpecifyKind(x, DateTimeKind.Utc), x => DateTime.SpecifyKind(x, DateTimeKind.Local))
+      public DateTimeConverter() : base(x => x.ToUniversalTime(), x => x.ToLocalTime())
       {
       }
    }
