e228f518c069c5c27f0c4c63aba03430f2e08106
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2023-11-25 18:45:22 +0100

    Change paths

--- scripts/Publish.Linux.Arm64.ps1
+++ scripts/Publish.Linux.Arm64.ps1
@@ -1,8 +1,8 @@
-dotnet publish "./src/Phoenix.Api.Client/Phoenix.Api.Client.csproj" -o "./dist/arm/phoenix.api.client" -c Release -r linux-arm64
-dotnet publish "./src/Phoenix.Api.Web/Phoenix.Api.Web.csproj" -o "./dist/arm/phoenix.api.web" -c Release -r linux-arm64
-dotnet publish "./src/Phoenix.Client/Phoenix.Client.csproj" -o "./dist/arm/phoenix.client" -c Release -r linux-arm64
+dotnet publish "./src/Phoenix.Api.Client/Phoenix.Api.Client.csproj" -o "./dist/linux/arm64/phoenix.api.client" -c Release -r linux-arm64
+dotnet publish "./src/Phoenix.Api.Web/Phoenix.Api.Web.csproj" -o "./dist/linux/arm64/phoenix.api.web" -c Release -r linux-arm64
+dotnet publish "./src/Phoenix.Client/Phoenix.Client.csproj" -o "./dist/linux/arm64/phoenix.client" -c Release -r linux-arm64
 
-Set-Location "./src/Phoenix.Web"
+Set-Location "./src/phoenix.web"
 yarn install
 yarn build
 Set-Location "../../"
---
+++ scripts/publish.linux.amd64.ps1
@@ -0,0 +1,8 @@
+dotnet publish "./src/Phoenix.Api.Client/Phoenix.Api.Client.csproj" -o "./dist/linux/amd64/phoenix.api.client" -c Release -r linux-x64
+dotnet publish "./src/Phoenix.Api.Web/Phoenix.Api.Web.csproj" -o "./dist/linux/amd64/phoenix.api.web" -c Release -r linux-x64
+dotnet publish "./src/Phoenix.Client/Phoenix.Client.csproj" -o "./dist/linux/amd64/phoenix.client" -c Release -r linux-x64
+
+Set-Location "./src/phoenix.web"
+yarn install
+yarn build
+Set-Location "../../"
