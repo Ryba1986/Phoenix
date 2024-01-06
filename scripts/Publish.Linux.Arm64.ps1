66e34fea58b2806fa541f00351af1d8640977501
Maciej Rybarczyk <mrybarczyk86@gmail.com>
2024-01-06 16:19:36 +0100

    Fix path

--- scripts/Publish.Linux.Arm64.ps1
+++ scripts/Publish.Linux.Arm64.ps1
@@ -2,7 +2,7 @@ dotnet publish "./src/Phoenix.Api.Client/Phoenix.Api.Client.csproj" -o "./dist/l
 dotnet publish "./src/Phoenix.Api.Web/Phoenix.Api.Web.csproj" -o "./dist/linux/arm64/phoenix.api.web" -c Release -r linux-arm64
 dotnet publish "./src/Phoenix.Client/Phoenix.Client.csproj" -o "./dist/linux/arm64/phoenix.client" -c Release -r linux-arm64
 
-Set-Location "./src/phoenix.web"
+Set-Location "./src/Phoenix.Web"
 yarn install
 yarn build
 Set-Location "../../"
--- scripts/publish.linux.amd64.ps1
+++ scripts/publish.linux.amd64.ps1
@@ -2,7 +2,7 @@ dotnet publish "./src/Phoenix.Api.Client/Phoenix.Api.Client.csproj" -o "./dist/l
 dotnet publish "./src/Phoenix.Api.Web/Phoenix.Api.Web.csproj" -o "./dist/linux/amd64/phoenix.api.web" -c Release -r linux-x64
 dotnet publish "./src/Phoenix.Client/Phoenix.Client.csproj" -o "./dist/linux/amd64/phoenix.client" -c Release -r linux-x64
 
-Set-Location "./src/phoenix.web"
+Set-Location "./src/Phoenix.Web"
 yarn install
 yarn build
 Set-Location "../../"
